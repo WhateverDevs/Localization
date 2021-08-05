469a5fcea46f818673e65a90b57f8dea948e67c2
Rodrigo Varga <rodrigo.varga@itcl.es>
2021-08-05 09:35:57 +0200

    - Cleaned up some code.

--- Runtime/Ui/LocalizedTextMeshPro.cs
+++ Runtime/Ui/LocalizedTextMeshPro.cs
@@ -1,7 +1,6 @@
-using System;
 using TMPro;
 using UnityEngine;
-using WhateverDevs.Core.Runtime.Common;
+using WhateverDevs.Core.Runtime.Ui;
 using Zenject;
 
 namespace WhateverDevs.Localization.Runtime.Ui
@@ -10,7 +9,7 @@ namespace WhateverDevs.Localization.Runtime.Ui
     /// MonoBehaviour that can be used to assign a localized text to a TMP Text through inspector.
     /// </summary>
     [RequireComponent(typeof(TMP_Text))]
-    public class LocalizedTextMeshPro : LoggableMonoBehaviour<LocalizedTextMeshPro>
+    public class LocalizedTextMeshPro : EasyUpdateText<LocalizedTextMeshPro>
     {
         /// <summary>
         /// Should the value be set on enable?
@@ -21,23 +20,6 @@ namespace WhateverDevs.Localization.Runtime.Ui
         /// Key to display.
         /// </summary>
         public string LocalizationKey;
-
-        /// <summary>
-        /// Reference to the TMP text.
-        /// </summary>
-        private TMP_Text Text
-        {
-            get
-            {
-                if (text == null) text = GetComponent<TMP_Text>();
-                return text;
-            }
-        }
-
-        /// <summary>
-        /// Backfield for Text.
-        /// </summary>
-        private TMP_Text text;
 
         /// <summary>
         /// Reference to the localizer.
@@ -72,6 +54,6 @@ namespace WhateverDevs.Localization.Runtime.Ui
         /// <summary>
         /// Called every time the language changes.
         /// </summary>
-        private void OnLanguageChanged() => Text.SetText(Localizer[LocalizationKey]);
+        private void OnLanguageChanged() => UpdateText(Localizer[LocalizationKey]);
     }
 }

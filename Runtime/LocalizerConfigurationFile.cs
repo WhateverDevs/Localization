d1c6dc202104b243e9a1721fb21b1befaf888820
Rodrigo Varga <varguiniano@gmail.com>
2023-10-07 09:54:37 +0200

    - Support for Core 0.3.0.

--- Runtime/LocalizerConfigurationFile.cs
+++ Runtime/LocalizerConfigurationFile.cs
@@ -33,5 +33,13 @@ namespace WhateverDevs.Localization.Runtime
         /// Currently selected language.
         /// </summary>
         public string SelectedLanguage = "English";
+
+        /// <summary>
+        /// Clone this data into a new instance of the same type.
+        /// </summary>
+        /// <typeparam name="TConfigurationData">Type of the configuration.</typeparam>
+        /// <returns>The cloned config.</returns>
+        protected override TConfigurationData Clone<TConfigurationData>() =>
+            new LocalizerConfiguration { SelectedLanguage = SelectedLanguage } as TConfigurationData;
     }
 }
